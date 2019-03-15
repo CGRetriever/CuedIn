ca3021a85f88ad558172aa3272476a714458b930
Alyssa Martin <marti5an@dukes.jmu.edu>
2019-03-15 16:33:38 -0400

    adding cells to tables dynamically

---
+++ CuedIn/WebSite1/JobPostings.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SchoolMaster.master" AutoEventWireup="true" CodeFile="JobPostings.aspx.cs" Inherits="JobPostings" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<asp:Table ID="tblJobPosting" runat="server"></asp:Table>
+
+ <script type ="text/javascript">
+     function addCells() {
+         var table = '';
+         var rows = 2;
+         var cols = 3;
+         for (var i = 0; i < 5; i++) {
+             table += '<tr>';
+             for (var c = 1; c <= cols; c++) {
+                 table += '<td>' + c + '</td>';
+             }
+             table += '</tr>';
+         }
+         document.write('<table border = 1>' + table + '</table>');
+     }
+ </script>   
+   
+</asp:Content>
+
---
+++ CuedIn/WebSite1/JobPostings.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class JobPostings : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        int count = 0;
+        String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+        System.Data.SqlClient.SqlConnection sc = new System.Data.SqlClient.SqlConnection(connectionString);
+        sc.Open();
+
+        System.Data.SqlClient.SqlCommand countJobPostings = new System.Data.SqlClient.SqlCommand();
+        countJobPostings.CommandText = "select count(JobListingID) from JobListing where approved = 'yes'";
+        countJobPostings.Connection = sc;
+
+        System.Data.SqlClient.SqlDataReader reader = countJobPostings.ExecuteReader();
+
+        while (reader.Read())
+        {
+            count = reader.GetInt32(0);
+        }
+
+        int colCnt = 3;
+        int rowCnt = count / colCnt;
+
+        for (int rowCtr = 1; rowCtr <= rowCnt; rowCtr++)
+        {
+            TableRow tableRow = new TableRow();
+            tblJobPosting.Rows.Add(tableRow);
+            for (int cellCtr = 1; cellCtr <= colCnt; cellCtr++)
+            {
+                TableCell tableCell = new TableCell();
+                tableCell.Text
+            }
+        }
+
+
+    }
+}
