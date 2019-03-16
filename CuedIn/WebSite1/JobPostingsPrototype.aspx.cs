cdbe130a132ce58a4f313dd5d2977a45e12c9764
Ryan Nigro <nigrora@dukes.jmu.edu>
2019-03-16 10:49:34 -0400

    This synced up the page navigateion Login -> Landing Page -> Job opps

--- CuedIn/WebSite1/JobPostingsPrototype.aspx.cs
+++ CuedIn/WebSite1/JobPostingsPrototype.aspx.cs
@@ -5,14 +5,10 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Login : System.Web.UI.Page
+public partial class JobPostingsPrototype : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
-    }
-    protected void btn_click(object sender, EventArgs e)
-    {
-        Response.Redirect("JobPostingsPrototype.aspx");
     }
 }
