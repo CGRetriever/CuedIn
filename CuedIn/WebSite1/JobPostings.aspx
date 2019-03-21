05351d130331ba09f3981f8aa7ac0bc39e00ac2e
Alyssa Martin <marti5an@dukes.jmu.edu>
2019-03-21 15:11:30 -0400

    job cards work

--- CuedIn/WebSite1/JobPostings.aspx
+++ CuedIn/WebSite1/JobPostings.aspx
@@ -4,26 +4,7 @@
     
     <form id="form1" runat="server">
     
-    <asp:GridView ID="jobPosting" runat="server" DataSourceID="jobPostingDB" AutoGenerateColumns="False">
-        <Columns>
-            <asp:TemplateField ItemStyle-CssClass="card-body card-body-cascade text-center">
-                <ItemTemplate> 
-                    <asp:Image ID="image" runat="server" ImageURL= CssClass="card-img-top" />
-                    <asp:Label ID ="company" runat="server" Text='<%# Bind("OrganizationName")%>' CssClass="h4"></asp:Label>
-                    <br />
-                    <asp:Label ID="subtitle" runat="server" Text='<%# Bind ("JobTitle")%>' CssClass="font-weight-bold indigo-text py-2 h6"></asp:Label>
-                    <br />
-                    <asp:Label ID="text" runat="server" Text='<%# Bind("JobDescription")%>' CssClass="p-0"></asp:Label>
-
-                </ItemTemplate>
-            </asp:TemplateField>
-                        
-        </Columns>
-    </asp:GridView>
-
-        <asp:SqlDataSource ID="jobPostingDB" runat="server" ConnectionString="<%$ ConnectionStrings:DBConnectionString %>" SelectCommand="SELECT JobListing.JobTitle, JobListing.JobDescription, Organization.OrganizationName FROM JobListing INNER JOIN Organization ON JobListing.OrganizationID = Organization.OrganizationEntityID WHERE UPPER(JobListing.Approved) = 'YES'"></asp:SqlDataSource>
-
-
+    <asp:Table ID="jobPostingTable" runat="server" OnLoad="jobPostingTable_Load" Width="100%" > </asp:Table>
    
     </form>
    
--- CuedIn/WebSite1/JobPostings.aspx.cs
+++ CuedIn/WebSite1/JobPostings.aspx.cs
@@ -10,9 +10,13 @@ public partial class JobPostings : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
-       
 
-        int count = 0;
+
+    }
+
+    protected void jobPostingTable_Load(object sender, EventArgs e)
+    {
+        int countTotalJobs = 0;
         String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
         System.Data.SqlClient.SqlConnection sc = new System.Data.SqlClient.SqlConnection(connectionString);
         sc.Open();
@@ -25,11 +29,69 @@ public partial class JobPostings : System.Web.UI.Page
 
         while (reader.Read())
         {
-            count = reader.GetInt32(0);
+            countTotalJobs = reader.GetInt32(0);
         }
 
-     
+        sc.Close();
+
+        
+
+        sc.Open();
+        System.Data.SqlClient.SqlCommand pullJobInfo = new System.Data.SqlClient.SqlCommand();
+        pullJobInfo.CommandText = "SELECT Organization.OrganizationName, JobListing.JobTitle, JobListing.JobDescription, JobListing.Image FROM JobListing INNER JOIN Organization ON JobListing.OrganizationID = Organization.OrganizationEntityID";
+        pullJobInfo.Connection = sc;
 
 
+
+        reader = pullJobInfo.ExecuteReader();
+
+        {
+            String[] orgNameArray = new String[countTotalJobs];
+            String[] jobTitleArray = new String[countTotalJobs];
+            String[] jobDescriptionArray = new String[countTotalJobs];
+            String[] imageArray = new string[countTotalJobs];
+            int x = 0;
+            while (reader.Read())
+            {
+                orgNameArray[x] = reader.GetString(0);
+                jobTitleArray[x] = reader.GetString(1);
+                jobDescriptionArray[x] = reader.GetString(2);
+                imageArray[x] = reader.GetString(3);
+                x++;
+
+            }
+            sc.Close();
+
+            int numrows = countTotalJobs / 3;
+            int numcells = 3;
+            int count = 0;
+            for (int j = 0; j < numrows; j++)
+            {
+            TableRow r = new TableRow();
+             
+               
+            for (int i = 0; i < numcells; i++)
+            {
+                TableCell c = new TableCell();
+                    c.Text += "<div class = 'card card-cascade>";
+                    c.Text += "<div class = 'view view-cascade overlay'>";
+                    c.Text += "<img class = 'card-img-top' src='" + imageArray[count] + "'>";
+                    c.Text += "<div class = 'card-body card-body-cascade text-center'>";
+                    c.Text += "<h4 class='card-title'> <strong>" + orgNameArray[count] + "</strong> </h4>";
+                    c.Text += "<div class='font-weight-bold indigo-text py-2'>" + jobTitleArray[count] + "</div>";
+                    c.Text += "<div class = 'card-text'>" + jobDescriptionArray[count] + "</div>";
+                    c.Text += "</div>";
+                    c.Text += "</div>";
+                    c.Text += "</div>";
+                    c.Text += "</div>";
+                    c.Style.Add("width", "33%");
+                    r.Cells.Add(c);                   
+                    count++;
+                }
+                jobPostingTable.Rows.Add(r);
+            }
+            
+        }
     }
+
 }
