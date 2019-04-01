c4b22aa81363194f57442a4cf8559b2b889d1370
Ryan Nigro <nigrora@dukes.jmu.edu>
2019-04-01 16:05:09 -0400

    Changed minor web config settings

--- CuedIn/WebSite1/SchoolMaster.master.cs
+++ CuedIn/WebSite1/SchoolMaster.master.cs
@@ -9,6 +9,13 @@ public partial class SchoolMaster : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //Response.Cache.SetExpires(DateTime.UtcNow.AddMinutes(-1));
+        //Response.Cache.SetCacheability(System.Web.HttpCacheability.NoCache);
+        //Response.Cache.SetNoStore();
+    }
 
+    protected void btn(object sender, EventArgs e)
+    {
+        //Response.Redirect("Login.aspx");
     }
 }
