648a5b278a904ac07293a938ee10a28848607581
Ryan Nigro <nigrora@dukes.jmu.edu>
2019-04-08 12:29:23 -0400

    Added Job Postings for OA

---
+++ CuedIn/WebSite1/OAJobPostings.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/OA.master" AutoEventWireup="true" CodeFile="OAJobPostings.aspx.cs" Inherits="OAJobPostings" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    
+    <form id="form1" runat="server">
+    <head>
+        <title>Job Postings</title>
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="keywords" content="">
+        <!-- Font Awesome for icons - see https://fontawesome.com/v4.7.0/cheatsheet/ -->
+        <link rel='stylesheet' href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <!-- Custom CSS - update this if you use a name other than style.css for the Sass-generated CSS -->
+        <link rel="stylesheet" href="css/style.css">
+         <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+         <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+
+    </head>
+            <!--Most Recent Job Posting-->
+
+            <div class='container'>     <!-- Main container div -->
+                <div class='row'>       <!-- Row start div -->
+                <div class='col-12 '>   <!-- Job Reccomend text div -->
+                    <p>Job Post Just Added</p>
+                </div>                  <!-- Close Job Reccomend text div -->
+            </div>                      <!-- Row close div -->
+                <div class='row'>     <!-- Start row Diiv -->
+                    <div class='col-md-4 col-sm-4 col-xs-4' id='ashvet'>  <!-- Start column div -->
+                       <div class='thumbnail'>          <!-- Start thumbnail div -->
+                            <img src='<%=orgImage %>' alt='' class='img-fluid'>
+    
+                        </div>          <!-- End thumbnail div -->
+                    </div>              <!-- End column div -->
+                    
+                    <div class ='col-md-4 col-sm-4 col-xs-4'>  <!-- Start column div -->
+                        <h4> <%=orgName%></h4>
+                        <p> <%=jobTitle %></p>
+                        <h5> <%=jobDescription%></h5>
+                    </div>             <!-- End column div -->
+                    <div class ='col-md-4 col-sm-4 col-xs-4'>  <!-- Start column div -->
+                        <div id='tie'> <!-- Start employeediv -->
+                        <i class='fab fa-black-tie'></i>
+                        <p><%=numOfApplicants%></p>
+                        </div>  <!-- Start column div -->
+                        <i class='far fa-clock'></i>
+                        <p><%=jobType%></p>
+                        <i class='fas fa-map-marker-alt'></i>
+                        <p><%=jobLocation%></p>
+                    </div>             <!-- End column div -->
+                </div><!-- End row Diiv -->
+                <div class='row'>       <!-- Row start div -->
+                <div class='col-12 '>   <!-- Job Reccomend text div -->
+                    <p>More Job Postings</p>
+                </div>                  <!-- Close Job Reccomend text div -->
+
+                <!--End of Recent Job Posting-->
+    
+                    
+                    <asp:Table ID="jobPostingTable" runat="server" OnLoad="jobPostingTable_Load" Width="100%" > </asp:Table>
+
+   
+    </form>
+   
+</asp:Content>
+
+
+
---
+++ CuedIn/WebSite1/OAJobPostings.aspx.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class OAJobPostings : System.Web.UI.Page
+{
+    public static int recentPostID = 0;
+    public String jobTitle = "";
+    public String jobDescription = "";
+    public String jobType = "";
+    public String jobLocation = "";
+    public DateTime jobDeadline = DateTime.Today;
+    public int numOfApplicants = 0;
+    public String orgName = "";
+    public String orgDescription = "";
+    public String orgImage = "";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+
+        String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+        System.Data.SqlClient.SqlConnection sc = new System.Data.SqlClient.SqlConnection(connectionString);
+        sc.Open();
+
+        System.Data.SqlClient.SqlCommand sqlrecentJobPostID = new System.Data.SqlClient.SqlCommand();
+        sqlrecentJobPostID.CommandText = "select max(joblistingID) from jobListing;";
+        sqlrecentJobPostID.Connection = sc;
+        System.Data.SqlClient.SqlDataReader reader = sqlrecentJobPostID.ExecuteReader();
+
+        while (reader.Read())
+        {
+            recentPostID = reader.GetInt32(0);
+        }
+
+        sc.Close();
+
+        sc.Open();
+
+
+        System.Data.SqlClient.SqlCommand recentJobPost = new System.Data.SqlClient.SqlCommand();
+        recentJobPost.CommandText = "SELECT JobListing.JobTitle, JobListing.JobDescription, JobListing.JobType, JobListing.Location, JobListing.Deadline, JobListing.NumOfApplicants, Organization.OrganizationName, Organization.OrganizationDescription, Organization.Image FROM JobListing INNER JOIN Organization ON JobListing.OrganizationID = Organization.OrganizationEntityID where JobListingID =" + recentPostID;
+        recentJobPost.Connection = sc;
+
+        reader = recentJobPost.ExecuteReader();
+
+        ((Label)Master.FindControl("lblMaster")).Text = "Job Cards";
+
+
+
+        while (reader.Read())
+        {
+            jobTitle = reader.GetString(0);
+            jobDescription = reader.GetString(1);
+            jobType = reader.GetString(2);
+            jobLocation = reader.GetString(3);
+            jobDeadline = reader.GetDateTime(4);
+            numOfApplicants = reader.GetInt32(5);
+            orgName = reader.GetString(6);
+            orgDescription = reader.GetString(7);
+            orgImage = reader.GetString(8);
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+    protected void jobPostingTable_Load(object sender, EventArgs e)
+    {
+        int countTotalJobs = 0;
+        String connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+        System.Data.SqlClient.SqlConnection sc = new System.Data.SqlClient.SqlConnection(connectionString);
+        sc.Open();
+
+        System.Data.SqlClient.SqlCommand countJobPostings = new System.Data.SqlClient.SqlCommand();
+        countJobPostings.CommandText = "select count(JobListingID) from JobListing where approved = 'Y' and jobListingID <> " + recentPostID;
+        countJobPostings.Connection = sc;
+
+        System.Data.SqlClient.SqlDataReader reader = countJobPostings.ExecuteReader();
+
+        while (reader.Read())
+        {
+            countTotalJobs = reader.GetInt32(0);
+        }
+
+        sc.Close();
+
+
+
+        sc.Open();
+        System.Data.SqlClient.SqlCommand pullJobInfo = new System.Data.SqlClient.SqlCommand();
+        pullJobInfo.CommandText = "SELECT Organization.OrganizationName, JobListing.JobTitle, JobListing.JobDescription, Organization.Image, Organization.ExternalLink FROM JobListing INNER JOIN Organization ON JobListing.OrganizationID = Organization.OrganizationEntityID where jobListingID <>" + recentPostID + "and approved = 'Y' ";
+        pullJobInfo.Connection = sc;
+
+
+
+        reader = pullJobInfo.ExecuteReader();
+
+        {
+            String[] orgNameArray = new String[countTotalJobs];
+            String[] jobTitleArray = new String[countTotalJobs];
+            String[] jobDescriptionArray = new String[countTotalJobs];
+            String[] imageArray = new string[countTotalJobs];
+            String[] linkArray = new string[countTotalJobs];
+            int x = 0;
+            while (reader.Read())
+            {
+                orgNameArray[x] = reader.GetString(0);
+                jobTitleArray[x] = reader.GetString(1);
+                jobDescriptionArray[x] = reader.GetString(2);
+                imageArray[x] = reader.GetString(3);
+                linkArray[x] = reader.GetString(4);
+                x++;
+
+            }
+            sc.Close();
+            double doubleRows = countTotalJobs / 3.0;
+            int numrows = (int)(Math.Ceiling(doubleRows));
+            int numcells = 3;
+            int count = 0;
+            for (int j = 0; j < numrows; j++)
+            {
+                TableRow r = new TableRow();
+
+
+                for (int i = 0; i < numcells; i++)
+                {
+                    if (count == countTotalJobs)
+                    {
+                        break;
+                    }
+                    TableCell c = new TableCell();
+                    c.Text += "<div class = 'card card-cascade>";
+                    c.Text += "<div class = 'view view-cascade overlay'>";
+                    c.Text += "<img class = 'card-img-top' src='" + imageArray[count] + "'>";
+                    c.Text += "<div class = 'card-body card-body-cascade text-center'>";
+                    c.Text += "<h4 class='card-title'> <strong>" + orgNameArray[count] + "</strong> </h4>";
+                    c.Text += "<div class='font-weight-bold indigo-text py-2'>" + jobTitleArray[count] + "</div>";
+                    c.Text += "<div class = 'card-text'>" + jobDescriptionArray[count] + "</div>";
+                    c.Text += "<a type ='button' class = 'border border-white btn-medium btn-round' style = 'background-color:#ffffff;' href='" + linkArray[count] + "' target = '_blank'><i class='fas fa-link' > </i></a>";
+                    c.Text += "</div>";
+                    c.Text += "</div>";
+                    c.Text += "</div>";
+                    c.Text += "</div>";
+                    c.Style.Add("width", "33%");
+                    r.Cells.Add(c);
+                    count++;
+
+                }
+                jobPostingTable.Rows.Add(r);
+            }
+
+
+        }
+
+    }
+}
+
+
+
+
+
