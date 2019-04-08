b6bd56ffdbce9e099a3c1e38a040f1b20f1c5971
Ryan Nigro <nigrora@dukes.jmu.edu>
2019-04-08 12:57:18 -0400

    Added Student Metrics Dashboard

---
+++ CuedIn/WebSite1/OAStudentMetricsDashboard.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class OAStudentMetricsDashboard : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        ((Label)Master.FindControl("lblMaster")).Text = "Administrative Dashboard";
+
+    }
+}
