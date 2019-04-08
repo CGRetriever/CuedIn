e182c4c9dd820d2f92f598cbf0cb13b39c95cf74
Ryan Nigro <nigrora@dukes.jmu.edu>
2019-04-08 11:50:41 -0400

    Added the master page for OA

---
+++ CuedIn/WebSite1/OA.master.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class OA : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["user"] == null || !Session["permission"].Equals("OA"))
+        {
+            Response.Redirect("Login.aspx");
+        }
+
+        else if (!Session.IsNewSession && Request.UrlReferrer == null)
+        {
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
