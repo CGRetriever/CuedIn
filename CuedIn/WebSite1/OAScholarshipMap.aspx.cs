b5ea28148ab95655fbc5ca700bb34c9c044bc3a9
Ryan Nigro <nigrora@dukes.jmu.edu>
2019-04-08 12:50:14 -0400

    added scholarship map to the OA

--- CuedIn/WebSite1/OAScholarshipMap.aspx.cs
+++ CuedIn/WebSite1/OAScholarshipMap.aspx.cs
@@ -5,12 +5,15 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class OAScholarshipDashboard : System.Web.UI.Page
+public partial class OAScholarshipMap : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
-        ((Label)Master.FindControl("lblMaster")).Text = "Scholarship Dashboard";
+
+
+        ((Label)Master.FindControl("lblMaster")).Text = "Scholarship Map";
+
 
     }
 }
