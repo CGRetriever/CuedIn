51ab0a2332525d21583f11f432aa4a42cd4091fa
Ryan Nigro <nigrora@dukes.jmu.edu>
2019-04-10 18:20:07 -0400

    commented out

---
+++ CuedIn/WebSite1/App_Code/TwilioObj.cs
@@ -0,0 +1,27 @@
+﻿// Install the C# / .NET helper library from twilio.com/docs/csharp/install
+
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+
+class TwilioObj
+{
+    static void Main(string[] args)
+    {
+        // Find your Account Sid and Token at twilio.com/console
+        // DANGER! This is insecure. See http://twil.io/secure
+        const string accountSid = "AC8037eb8af9379976245f3cf6f232ab66";
+        const string authToken = "8bf06472bc5fc06603aa06068e7e252f";
+
+        TwilioClient.Init(accountSid, authToken);
+
+        var message = MessageResource.Create(
+            body: "Join Earth's mightiest heroes. Like Kevin Bacon.",
+            from: new Twilio.Types.PhoneNumber("+15402534874"),
+            to: new Twilio.Types.PhoneNumber("+16316268854")
+        );
+
+        Console.WriteLine(message.Sid);
+    }
+}
