1606f6d067b488d96a7941a18006bbb0b4b0fa2d
Ryan Nigro <nigrora@dukes.jmu.edu>
2019-04-10 23:40:10 -0400

    progresss

--- CuedIn/WebSite1/App_Code/Scheduler.cs
+++ CuedIn/WebSite1/App_Code/Scheduler.cs
@@ -10,17 +10,18 @@ namespace Quartz1 {
         {
 
             IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
             IJobDetail job = JobBuilder.Create<TwilioObj>().Build();
             ITrigger trigger = TriggerBuilder.Create()
              .WithDailyTimeIntervalSchedule
               (s =>
                   s.WithIntervalInHours(24)
                   .OnEveryDay()
-                  .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(23, 19))
+                  .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(23, 40))
                 
               )
              .Build();
-            System.Diagnostics.Debug.WriteLine("hey");
+            System.Diagnostics.Debug.WriteLine("hjbjkbey");
             scheduler.ScheduleJob(job, trigger);
         }
 
--- CuedIn/WebSite1/Global.asax
+++ CuedIn/WebSite1/Global.asax
@@ -2,13 +2,18 @@
 <%@ Import Namespace="WebSite1" %>
 <%@ Import Namespace="System.Web.Optimization" %>
 <%@ Import Namespace="System.Web.Routing" %>
+<%@ Import Namespace="Quartz1" %>
+
 
 <script runat="server">
 
     void Application_Start(object sender, EventArgs e)
     {
+
         RouteConfig.RegisterRoutes(RouteTable.Routes);
         BundleConfig.RegisterBundles(BundleTable.Bundles);
+        Scheduler sch = new Scheduler();
+        sch.Start(DateTime.Now);
     }
 
 </script>
