147184ad9e7e9a90d5fd8b5c29090f750764c508
Ryan Nigro <nigrora@dukes.jmu.edu>
2019-04-16 13:03:24 -0400

    added differnt stuff

--- CuedIn/WebSite1/StudentMetricsDashboard.aspx.cs
+++ CuedIn/WebSite1/StudentMetricsDashboard.aspx.cs
@@ -9,7 +9,19 @@ public partial class StudentMetricsDashboard : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+        if (Session["schoolid"].Equals(12))
+        {
+            lousia.Visible = true;
+        }
+        else if (Session["schoolid"].Equals(13))
+        {
+            rockingham.Visible = true;
+        }
+
+        else if (Session["schoolid"].Equals(15))
+        {
+            turner.Visible = true;
+        }
         ((Label)Master.FindControl("lblMaster")).Text = "Administrative Dashboard";
         
     }
