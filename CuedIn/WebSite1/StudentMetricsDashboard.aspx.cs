367542150566585d0ddff9fdb07c440c9f818ebe
Kenny Phan <phankq@dukes.jmu.edu>
2019-04-22 21:41:56 -0400

    Export Button Inserted

--- CuedIn/WebSite1/StudentMetricsDashboard.aspx.cs
+++ CuedIn/WebSite1/StudentMetricsDashboard.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -36,5 +39,58 @@ public partial class StudentMetricsDashboard : System.Web.UI.Page
         ((Label)Master.FindControl("lblMaster")).Text = "Administrative Dashboard";
         ((Label)Master.FindControl("lblMaster")).Attributes.Add("Style", "color: #fff; text-align:center; text-transform: uppercase; letter-spacing: 6px; font-size: 2.0em; margin: .67em");
 
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string constr = ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(constr))
+        {
+            using (SqlCommand cmd = new SqlCommand("SELECT Student.FirstName +' '+ Student.LastName as 'Student Name', Student.HoursOfWorkPlaceExp, LogHours.HoursRequested ,Student.HoursOfWorkPlaceExp +' '+ LogHours.HoursRequested as 'Total Hours', Student.StudentGradeLevel, Student.StudentEthnicity, Student.StudentGender, Student.IncomeLevel,     Student.StudentAthleteFlag FROM Student INNER JOIN   LogHours ON Student.StudentEntityID = LogHours.StudentEntityID"))  //insert our query here
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    sda.SelectCommand = cmd;
+                    using (DataTable dt = new DataTable())
+                    {
+                        sda.Fill(dt);
+                        //Build the CSV file data as a Comma separated string.
+                        string csv = string.Empty;
+
+                        foreach (DataColumn column in dt.Columns)
+                        {
+                            //Add the Header row for CSV file.
+                            csv += column.ColumnName + ',';
+                        }
+
+                        //Add new line.
+                        csv += "\r\n";
+
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            foreach (DataColumn column in dt.Columns)
+                            {
+                                //Add the Data rows.
+                                csv += row[column.ColumnName].ToString().Replace(",", ";") + ',';
+                            }
+
+                            //Add new line.
+                            csv += "\r\n";
+                        }
+
+                        //Download the CSV file.
+                        Response.Clear();
+                        Response.Buffer = true;
+                        Response.AddHeader("content-disposition", "attachment;filename=SqlExport.csv");
+                        Response.Charset = "";
+                        Response.ContentType = "application/text";
+                        Response.Output.Write(csv);
+                        Response.Flush();
+                        Response.End();
+                    }
+                }
+            }
+        }
     }
 }
