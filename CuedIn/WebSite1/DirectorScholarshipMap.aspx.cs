31ebdd7522e1c9b5dc5b2bec0a848d507bc70eba
Ryan Nigro <nigrora@dukes.jmu.edu>
2019-04-24 23:46:11 -0400

    xczv

---
+++ CuedIn/WebSite1/DirectorScholarshipMap.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DirectorScholarshipMap : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        ((Label)Master.FindControl("lblMaster")).Text = "Scholarship Map";
+        ((Label)Master.FindControl("lblMaster")).Attributes.Add("Style", "color: #fff; text-align:center; text-transform: uppercase; letter-spacing: 6px; font-size: 2.0em; margin: .67em");
+
+    }
+}
