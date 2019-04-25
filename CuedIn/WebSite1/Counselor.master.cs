82d891a3b7a57fd6e774f91cef011852908774bc
Ryan Nigro <nigrora@dukes.jmu.edu>
2019-04-25 00:15:51 -0400

    commented and added

--- CuedIn/WebSite1/Counselor.master.cs
+++ CuedIn/WebSite1/Counselor.master.cs
@@ -9,15 +9,15 @@ public partial class Counselor : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (Session["user"] == null || !Session["permission"].Equals("Advisor"))
-        {
-            Response.Redirect("Login.aspx");
-        }
+        //if (Session["user"] == null || !Session["permission"].Equals("Advisor"))
+        //{
+        //    Response.Redirect("Login.aspx");
+        //}
 
-        else if (!Session.IsNewSession && Request.UrlReferrer == null)
-        {
-            Response.Redirect("Login.aspx");
-        }
+        //else if (!Session.IsNewSession && Request.UrlReferrer == null)
+        //{
+        //    Response.Redirect("Login.aspx");
+        //}
 
         Session["schoolID"] = 12;
         Session["userCounty"] = "Harrisonburg City Public Schools";
---
+++ CuedIn/WebSite1/DirectorJobListingMap.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DirectorJobListingMap : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        ((Label)Master.FindControl("lblMaster")).Text = "Work Based Learning Map";
+        ((Label)Master.FindControl("lblMaster")).Attributes.Add("Style", "color: #fff; text-align:center; text-transform: uppercase; letter-spacing: 6px; font-size: 2.0em; margin: .67em");
+
+    }
+}
